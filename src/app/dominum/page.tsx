414b101ba505f94b9de32c747f37bf3de33f6854
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-04-12 01:48:23 +0300

    0.2.3

--- src/app/dominum/page.tsx
+++ src/app/dominum/page.tsx
@@ -21,21 +21,24 @@ export default function DominumPage() {
           resource="food"
           amount={50}
           icon="/icons/resources/food.png"
-          cooldownMs={1000 * 60 * 60 }
+          cooldownMs={1 * 60 * 60 }
+          position={{ bottom: "30%", right: "30%" }}
         />
 
         <ResourcesBonus
           resource="gold"
           amount={10}
           icon="/icons/resources/gold.png"
-          cooldownMs={1000 * 60 * 60 * 4}
+          cooldownMs={1 * 60 * 60 * 4}
+          position={{ top: "43%", left: "35%" }}
         />
 
         <ResourcesBonus
           resource="wood"
           amount={30}
           icon="/icons/resources/wood.png"
-          cooldownMs={1000 * 60 * 60 * 2}
+          cooldownMs={1 * 60 * 60 * 2}
+          position={{ top: "50%", left: "15%" }}
         />
 
       </div>
