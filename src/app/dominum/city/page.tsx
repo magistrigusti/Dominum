8c360b7c7f64e157bce69a736d647e257456c5fa
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-04-14 05:36:00 +0300

    0.2.7

--- src/app/dominum/city/page.tsx
+++ src/app/dominum/city/page.tsx
@@ -9,16 +9,43 @@ export default function CityPage() {
   const router = useRouter();
 
   return (
-    <div className={styles.city_wrapper}>
+    <div className={styles.page_wrapper}>
       <DOMHeader />
 
-      <img
-        src="/dominum/backGround/city_bg.png"
-        alt="city background"
-        className={styles.city_image}
-      />
+      <div className={styles.main_content}>
+        <img
+          src="/dominum/backGround/city_bg.png"
+          alt="city background"
+          className={styles.city_image}
+        />
 
-      {/* иконки будут поверх картинки */}
+        <div className={styles.icon_wrapper}>
+          <img
+            src="/img/castle_icon.png"
+            alt="Замок"
+            className={styles.icon}
+            style={{ top: '18%', left: '18%' }}
+          />
+          <img
+            src="/img/forge_icon.png"
+            alt="Кузня"
+            className={styles.icon}
+            style={{ top: '52%', left: '58%' }}
+          />
+          <img
+            src="/img/dock_icon.png"
+            alt="Причал"
+            className={styles.icon}
+            style={{ top: '70%', left: '25%' }}
+          />
+          <img
+            src="/img/auction_icon.png"
+            alt="Аукцион"
+            className={styles.icon}
+            style={{ top: '35%', left: '35%' }}
+          />
+        </div>
+      </div>
 
       <DOMFooter />
     </div>
