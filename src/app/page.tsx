fd0b68b1aa330aba86c689f6122c6c0b15ce65be
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-04-24 11:36:51 +0300

    0.3.4.04

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
   return (
     <div className={styles.login_bg_wrapper}>
       <div className={styles.login_inner}>
-        <h4 className={styles.login_title}>Dominum Space</h4>
+        <h4 className={styles.login_title}>Dominum™ Space</h4>
         <img src="/img/contract_actral_island.png" alt="" />
 
         <p className={styles.login_text}>
